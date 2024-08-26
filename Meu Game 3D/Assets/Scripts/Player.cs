ae4137dea3ad924ea4a949ca1bce70beb8cd6175
rubentarso <ruben.tarso@escolar.ifrn.edu.br>
2024-08-26 16:17:47 -0300

    moeda feita, porem nao coletavel

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
+    public bool noChao;
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -13,6 +16,14 @@ public class Player : MonoBehaviour
     {
       Debug.Log("START");
       TryGetComponent(out rb);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChao && collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
     }
 
     // Update is called once per frame
@@ -26,6 +37,14 @@ public class Player : MonoBehaviour
          Vector3 direcao = new Vector3(h, 0, v);
          rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
 
+
+         if (Input.GetKeyDown(KeyCode.Space) && noChao)
+         {
+             rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+             noChao = false;
+         }
+         
+         
          if (transform.position.y <= -10)
          {
              //jogador caiu
